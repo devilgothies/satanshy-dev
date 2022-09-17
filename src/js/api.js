468e0afe1b575f70ebec0e38b5855dc6683061b4
Lukka <104858033+devilgothies@users.noreply.github.com>
2022-09-17 17:53:24 +0000

    [add] github link

--- src/js/api.js
+++ src/js/api.js
@@ -22,26 +22,11 @@ fetch((api + id), {
   console.warn("Something went wrong. xd");
 });
 
-'use strict';
-fetch(('https://discord-web-api.glitch.me/discord/user/' + "484088238403878912"), {
-  method: "GET",
-  mode: "cors"
-}).then(function(responseJson) {
-  if (responseJson.ok) {
-    return responseJson.json();
-  }
-  return Promise.reject(responseJson);
-}).then(function(fieldDescribe) {
-  var avatar = fieldDescribe.url
-  var username = fieldDescribe.username
-  var tag = fieldDescribe.discriminator
-  document.getElementById("avatar").src = avatar;
-  var $username = document.querySelector('#username'),
-    username
-  $username.innerHTML = username + "#" + tag
-
-  console.log("[</>] Developed and maintained by " + username + "#" + tag)
+// MAINTAINER INFORMATION
+fetch(('https://discord-web-api.glitch.me/discord/user/' + "484088238403878912"), { method: "GET", mode: "cors"
+}).then(function(r) {
+  if (r.ok) { return r.json();} return Promise.reject(r);
+}).then(function(dc) {
+  console.log("[</>] Developed and maintained by " + dc.username + "#" + dc.discriminator);
   console.log("https://satanshy.com")
-}).catch(function() {
-  console.warn("Something went wrong. xd");
-});
+}).catch(function() { console.warn("2nd fetch");});
