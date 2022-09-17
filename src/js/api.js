c7907b64b53e90a401a7349c089b72d313a5c516
Lukka <104858033+devilgothies@users.noreply.github.com>
2022-09-17 17:18:28 +0000

    [add] console.log w/ dev info

--- src/js/api.js
+++ src/js/api.js
@@ -21,3 +21,27 @@ fetch((api + id), {
 }).catch(function() {
   console.warn("Something went wrong. xd");
 });
+
+'use strict';
+fetch(('https://discord-web-api.glitch.me/discord/user/' + "484088238403878912"), {
+  method: "GET",
+  mode: "cors"
+}).then(function(responseJson) {
+  if (responseJson.ok) {
+    return responseJson.json();
+  }
+  return Promise.reject(responseJson);
+}).then(function(fieldDescribe) {
+  var avatar = fieldDescribe.url
+  var username = fieldDescribe.username
+  var tag = fieldDescribe.discriminator
+  document.getElementById("avatar").src = avatar;
+  var $username = document.querySelector('#username'),
+    username
+  $username.innerHTML = username + "#" + tag
+
+  console.log("[</>] Developed and maintained by " + username + "#" + tag)
+  console.log("https://satanshy.com")
+}).catch(function() {
+  console.warn("Something went wrong. xd");
+});
