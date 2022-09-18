58aff7c999d4240a6e112389a955472d865775e6
Lukka <104858033+devilgothies@users.noreply.github.com>
2022-09-18 14:10:51 -0300

    [add] github link

--- src/js/links.js
+++ src/js/links.js
@@ -4,6 +4,7 @@ const instagram = () => (window.open("https://instagram.com/devilgothies"));
 const twitter = () => (window.open("https://twitter.com/devilgothies"));
 const spotify = () => (window.open("https://open.spotify.com/user/e4pvmol2nkp75123l641l9zbc?si=94b81d96719444ea"));
 const pinterest = () => (window.open("https://br.pinterest.com/devilnotcry/"));
+const github = () => (window.open("https://github.com/devilgothies/"));
 
 // OTHERS
 
